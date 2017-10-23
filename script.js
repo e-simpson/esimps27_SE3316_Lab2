9a8d59fd4d2e885f85cd1eaf80d3a803adae7d63
Evan Simpson <esimps27@uwo.ca>
2017-10-23 17:43:29 +0000

    line endings

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 /*global $*/
-var fruits = ["Pineapple", "Guapple", "Apple", "Starfruit", "Loquat"];
+var fruits = ["Pineapple", "Guapple", "Apple", "Starfruit", "Loquat"]
 
 $(document).ready(function(){
     for (var f in fruits){
-        $('#fruits').append('<li>' + fruits[f] + '</li>');
+        $('#fruits').append('<li>' + fruits[f] + '</li>')
     }
    
     $( "#enter" ).click(function() {
-        var text = $('#enterFruit').val();
-        var find = ($('#fruits > li:contains("' + text + '")' ));
-        find = find.filter(function() {return $(this).text() === text });
-        $('#basket').append(find);
-        $('#enterFruit').val('');
+        var text = $('#enterFruit').val()
+        var find = ($('#fruits > li:contains("' + text + '")' ))
+        find = find.filter(function() {return $(this).text() === text })
+        $('#basket').append(find)
+        $('#enterFruit').val('')
     });
     
     $( "#enterFruit" ).keyup(function (e) {
-        if (e.which == 13) { $("#enter").click();}
+        if (e.which == 13) { $("#enter").click()}
     });
 });
